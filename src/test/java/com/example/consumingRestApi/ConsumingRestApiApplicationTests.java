c9d073af37b3a515b6d6e61e2decdd829c2a8b5c
omkarkhambekar <omkarkncodeit@gmail.com>
2023-09-06 12:56:31 +0530

    first commit

---
+++ src/test/java/com/example/consumingRestApi/ConsumingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumingRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
